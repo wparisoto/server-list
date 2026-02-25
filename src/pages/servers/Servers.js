3175907c8f4ec42b3e0985b42cacbb370d40cfc9
Wagner Parisoto <wagner.parisoto@desbravador.com.br>
2026-02-25 17:15:07 -0300

    add css styles

--- src/pages/servers/Servers.js
+++ src/pages/servers/Servers.js
@@ -6,130 +6,130 @@ import { db } from "../../firebase/config";
 import { addDoc, updateDoc, doc, collection, getDocs, onSnapshot } from "firebase/firestore";
 
 const Servers = () => {
-     const [loading, setLoading] = useState(true);
-     const [changedOtherSession, setChangedOtherSession] = useState(false);
-     const [dados, setDados] = useState({id: null, servers: []});
- 
-     const updateReg = (server) => {
-      let index = dados.servers.findIndex(s => s.id === server.id);
-      dados.servers[index].nome = server.nome
-      dados.servers[index].projeto = server.projeto
-      dados.servers[index].descricao = server.descricao
-        
-      setDados({...dados})  
-      console.log(dados)
-     }
+  const [loading, setLoading] = useState(true);
+  const [changedOtherSession, setChangedOtherSession] = useState(false);
+  const [dados, setDados] = useState({ id: null, servers: [] });
 
-     const deleteReg = (server) => {
-      let index = dados.servers.findIndex(s => s.id === server.id);
-      dados.servers.splice(index, 1)
-      setDados({...dados})
-     }
+  const updateReg = (server) => {
+    let index = dados.servers.findIndex(s => s.id === server.id);
+    dados.servers[index].nome = server.nome
+    dados.servers[index].projeto = server.projeto
+    dados.servers[index].descricao = server.descricao
 
- 
-     const fetchPost = async () => {
+    setDados({ ...dados })
+    console.log(dados)
+  }
 
-        onSnapshot(collection(db, "dados"), { includeMetadataChanges: false }, (doc) => {
-          doc.docChanges().forEach((change) => {
-            if (!doc.metadata.hasPendingWrites && change.type === 'modified') {
-              console.log('Documento modificado');
-              setChangedOtherSession(true)
-            }
-          });
+  const deleteReg = (server) => {
+    let index = dados.servers.findIndex(s => s.id === server.id);
+    dados.servers.splice(index, 1)
+    setDados({ ...dados })
+  }
 
+
+  const fetchPost = async () => {
+
+    onSnapshot(collection(db, "dados"), { includeMetadataChanges: false }, (doc) => {
+      doc.docChanges().forEach((change) => {
+        if (!doc.metadata.hasPendingWrites && change.type === 'modified') {
+          console.log('Documento modificado');
+          setChangedOtherSession(true)
+        }
       });
 
-         await getDocs(collection(db, "dados"))
-             .then((querySnapshot)=>{               
-              
-              setLoading(false)
+    });
 
-              const newData = querySnapshot.docs.map((doc) => ({
-                
-                id:doc.id,   
-                ...doc.data()
-                  }));
+    await getDocs(collection(db, "dados"))
+      .then((querySnapshot) => {
 
-                  if(newData && newData.length > 0){
-                    setDados(newData[0]);                
-                    console.log(newData[0]);
-                  }
-             }).catch(err =>{
-              console.log(err)
-             })
+        setLoading(false)
+
+        const newData = querySnapshot.docs.map((doc) => ({
+
+          id: doc.id,
+          ...doc.data()
+        }));
+
+        if (newData && newData.length > 0) {
+          setDados(newData[0]);
+          console.log(newData[0]);
+        }
+      }).catch(err => {
+        console.log(err)
+      })
 
 
-    
 
-     }
-    
-     useEffect(()=>{
-         fetchPost();
-     }, [])
 
-  
-     const handleAddDoc = async (e) => {
-      e.preventDefault();  
+  }
 
-      let ids = dados.servers.map(s => s.id)
-      let maiorId = Math.max.apply(null, ids );
+  useEffect(() => {
+    fetchPost();
+  }, [])
 
-      dados.servers.push({
-        id: (maiorId > 0 ? maiorId: 0) + 1, 
-        nome: '', 
-        projeto: '',
-        descricao: ''
-      }) 
 
-      let obj = {
-        ...dados
-      }
-      setDados(obj)
+  const handleAddDoc = async (e) => {
+    e.preventDefault();
+
+    let ids = dados.servers.map(s => s.id)
+    let maiorId = Math.max.apply(null, ids);
+
+    dados.servers.push({
+      id: (maiorId > 0 ? maiorId : 0) + 1,
+      nome: '',
+      projeto: '',
+      descricao: ''
+    })
+
+    let obj = {
+      ...dados
     }
+    setDados(obj)
+  }
 
-    const handleUpdateDoc = async (e) => {
-      e.preventDefault();  
-      setLoading(true)
-      if(dados.id){
-          const examcollref = doc(db,'dados', dados.id)
-          updateDoc(examcollref, dados).then(response => {
-            setLoading(false)
-          }).catch(error =>{
-            console.log(error.message)
-            setLoading(false)
-          })
-      }else{
-          try {
-            const docRef = await addDoc(collection(db, "dados"), {
-              servers: dados.servers,    
-            });
-            console.log("Document written with ID: ", docRef.id);
-            setLoading(false)
-          } catch (e) {
-            console.error("Error adding document: ", e);
-            setLoading(false)
-          }
+  const handleUpdateDoc = async (e) => {
+    e.preventDefault();
+    setLoading(true)
+    if (dados.id) {
+      const examcollref = doc(db, 'dados', dados.id)
+      updateDoc(examcollref, dados).then(response => {
+        setLoading(false)
+      }).catch(error => {
+        console.log(error.message)
+        setLoading(false)
+      })
+    } else {
+      try {
+        const docRef = await addDoc(collection(db, "dados"), {
+          servers: dados.servers,
+        });
+        console.log("Document written with ID: ", docRef.id);
+        setLoading(false)
+      } catch (e) {
+        console.error("Error adding document: ", e);
+        setLoading(false)
       }
     }
+  }
 
 
 
-    return (
-      <div className={changedOtherSession && styles.disabled}>
-          {changedOtherSession && <span style={{fontSize: "20px"}}>Form updated in another session. Please reload!</span>}
-          <div className={styles.container}>
-                {dados && dados.servers && dados.servers.map((server, index) => <Server key={index} server={server} updateReg={updateReg} deleteReg={deleteReg}></Server>)}  
-          </div>
-          {loading && <span>Waiting...</span>}
-          
-          {!changedOtherSession && !loading && (
-            <div>
-              <button onClick={handleUpdateDoc} disabled={loading}>Save</button>
-              <button onClick={handleAddDoc} disabled={loading}>Add</button>
-          </div>
-          )} 
+  return (
+    <div className={changedOtherSession && styles.disabled}>
+      {changedOtherSession && <span style={{ fontSize: "20px" }}>Form updated in another session. Please reload!</span>}
+      <div className={styles.container}>
+        {dados && dados.servers && dados.servers.map((server, index) => <Server key={index} server={server} updateReg={updateReg} deleteReg={deleteReg}></Server>)}
       </div>
-    );
-  };
-  
-  export default Servers;
+      {loading && <span>Waiting...</span>}
+
+      {!changedOtherSession && !loading && (
+        <div>
+          <button className={styles.btnSave} onClick={handleUpdateDoc} disabled={loading}>Save</button>
+          <button className={styles.btnAdd} onClick={handleAddDoc} disabled={loading}>Add</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Servers;
